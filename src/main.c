eaa537371b2e0d6d93516505bf427301baf83903
Jared Woolston <Jared.Woolston@jacobs.com>
2021-03-08 16:41:43 -0800

    Redefines GPI pin connector in board DTS file and adds SPI1.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,19 @@
-#include <zephyr.h>
+#include "network.h"
 #include <device.h>
 #include <devicetree.h>
 #include <stdio.h>
-#include "network.h"
+#include <zephyr.h>
+
+K_THREAD_DEFINE(dhcp_mdns, DHCP_MDNS_THREAD_STACKSIZE, initialize_dhcp, NULL,
+                NULL, NULL, DHCP_MDNS_THREAD_PRIORITY, 0, 0);
 
 void main() {
-    printf("Starting main.\n");
+  printf("Starting main.\n");
 
-    initialize_dhcp();
-    int i = 0;
-    while (true) {
-    	printf("Loop %d", i++);
-        k_sleep(K_MSEC(16));
-    }
+  initialize_network();
+  int i = 0;
+  while (true) {
+    printf("Loop %d", i++);
+    k_sleep(K_MSEC(16));
+  }
 }
--- src/network.h
+++ src/network.h
@@ -6,4 +6,14 @@ LOG_MODULE_REGISTER(network, LOG_LEVEL_DBG);
 
 #include "dhcp.h"
 
+/* size of stack area used by each thread */
+#define DHCP_MDNS_THREAD_STACKSIZE 1024
+
+/* scheduling priority used by the thread */
+#define DHCP_MDNS_THREAD_PRIORITY 7
+
+void initialize_network() {
+  initialize_dhcp();
+}
+
 #endif // TEKDAQC_ZEPHYR_NETWORK_H

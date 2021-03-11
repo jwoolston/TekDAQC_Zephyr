ef09e92610038b99ae13dbdbe000315e28874169
Jared Woolston <Jared.Woolston@jacobs.com>
2021-03-11 15:25:50 -0800

    File system demo fixes.

--- src/main.c
+++ src/main.c
@@ -56,10 +56,10 @@ void main() {
       LOG_ERR("Unable to get sector size");
       break;
     }
-    printk("Sector size %u\n", block_size);
+    LOG_DBG("Sector size %u", block_size);
 
     memory_size_mb = (uint64_t)block_count * block_size;
-    printk("Memory Size(MB) %u\n", (uint32_t)(memory_size_mb >> 20));
+    LOG_DBG("Memory Size(MB) %u", (uint32_t)(memory_size_mb >> 20));
   } while (0);
 
   mp.mnt_point = disk_mount_pt;
@@ -67,10 +67,22 @@ void main() {
   int res = fs_mount(&mp);
 
   if (res == FR_OK) {
-    printk("Disk mounted.\n");
+    LOG_DBG("Disk mounted.\n");
     lsdir(disk_mount_pt);
+    LOG_DBG("Finished listing directory.");
+    const char* fname = "/SD:/test.txt";
+    res = fs_stat(fname, NULL);
+    struct fs_file_t file;
+    if (res == 0) {
+      res = fs_open(&file, fname, FS_O_CREATE | FS_O_WRITE);
+      if (res < 0) {
+        LOG_ERR("FAIL: open %s: %d\n", fname, res);
+      } else {
+        fs_close(&file);
+      }
+    }
   } else {
-    printk("Error mounting disk.\n");
+    LOG_ERR("Error mounting disk.\n");
   }
 
   int i = 0;
@@ -91,11 +103,11 @@ static int lsdir(const char *path) {
   /* Verify fs_opendir() */
   res = fs_opendir(&dirp, path);
   if (res) {
-    printk("Error opening dir %s [%d]\n", path, res);
+    LOG_ERR("Error opening dir %s [%d]\n", path, res);
     return res;
   }
 
-  printk("\nListing dir %s ...\n", path);
+  LOG_DBG("Listing dir %s ...", path);
   for (;;) {
     /* Verify fs_readdir() */
     res = fs_readdir(&dirp, &entry);
@@ -106,9 +118,9 @@ static int lsdir(const char *path) {
     }
 
     if (entry.type == FS_DIR_ENTRY_DIR) {
-      printk("[DIR ] %s\n", entry.name);
+      LOG_DBG("[DIR ] %s\n", entry.name);
     } else {
-      printk("[FILE] %s (size = %zu)\n", entry.name, entry.size);
+      LOG_DBG("[FILE] %s (size = %zu)\n", entry.name, entry.size);
     }
   }
 

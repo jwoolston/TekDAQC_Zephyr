30cf3f7fc007cd77d609fb0ea84e26f379c48236
Jared Woolston <Jared.Woolston@jacobs.com>
2021-03-13 20:25:02 -0800

    System time is set with SNTP.

--- src/sntp.h
+++ src/sntp.h
@@ -7,15 +7,15 @@
 #include <net/sntp.h>
 #include <posix/time.h>
 
-#define SERVER_ADDR "192.168.0.191" //"time.nist.gov"
+#define SNTP_SERVER_ADDR "192.168.0.191" //"time.nist.gov"
+#define SNTP_TIMEOUT 3000
 
 void init_clock_via_sntp(void) {
   LOG_MODULE_DECLARE(network);
   LOG_INF("Syncing SNTP Time.");
   struct sntp_time ts;
   struct timespec tspec;
-  int res = sntp_simple(SERVER_ADDR,
-                        CONFIG_NET_CONFIG_SNTP_INIT_TIMEOUT, &ts);
+  int res = sntp_simple(SNTP_SERVER_ADDR, SNTP_TIMEOUT, &ts);
 
   if (res < 0) {
     LOG_ERR("Cannot fetch time using SNTP: %d", res);
